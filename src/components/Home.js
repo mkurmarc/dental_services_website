cc21feaa019f8d1ba61f9b999dd9b24119c8ca46
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2022-06-06 20:54:46 -0700

    fix(): image name

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
-import keysplit_hard from "../images/raw/keyprint/keysplint_hard.png";
-import keysplit_soft from "../images/raw/keyprint/keysplint_soft.png";
+import keysplint_hard from "../images/raw/keyprint/keysplint_hard.png";
+import keysplint_soft from "../images/raw/keyprint/keysplint_soft.png";
 import cleaner_icon from "../images/raw/keyprint/easily_cleaned_icon.png";
 import bio_icon from "../images/raw/keyprint/biocompatible_icon.png";
 import strong_icon from "../images/raw/keyprint/strong_icon.png";
@@ -27,7 +27,7 @@ function HomeCardOne() {
             <h2 >3-D PRINTED TECHNOLOGY</h2>
             <p>The next evolutionary step in occusal splints has arrived. Digitally
                 designed and 3-D printed for maximum precision.</p>
-            <img id="home_card_one--img" src={keysplit_hard}></img>
+            <img id="home_card_one--img" src={keysplint_hard}></img>
             <h3>KEYSPLINT HARD</h3>
         </section>
     )
@@ -64,7 +64,7 @@ function HomeCardTwo() {
 function HomeCardThree() {
     return (
         <section className="home_card_three--container">
-            <img id="home_card_three--img" src={keysplit_soft}></img>
+            <img id="home_card_three--img" src={keysplint_soft}></img>
             <h3>KEYSPLINT SOFT</h3>
             <h2 >ELASTIC MEMORY BENEFITS ALL</h2>
             <p>By simply warming in water, placement become easy, fast and painless. Less
