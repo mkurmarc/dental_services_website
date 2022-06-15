1808d03797ee7431c3b98bafa2b1d96a551ab1fc
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2022-06-15 15:29:02 -0700

    build(): installed react-router, created About and Products components, and created working routes for each

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import "./style.css";
+import { BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
+import About from "./components/About";
+import Products from "./components/Products";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Home />
-      <Footer />
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/test" element={(
+            <div>
+              <h1>Test Page</h1>
+              <p>Practicing inline Route path</p>
+            </div>
+          )} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+      
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function About() {
+    return(
+        <main>
+            <h1>About Page</h1>
+        </main>
+    );
+}
